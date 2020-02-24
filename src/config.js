9bbf364b3819ea8cc77be01e859ac9deaf307a73
John Laine <john.laine5896@gmail.com>
2020-02-24 14:31:39 -0700

    Setting up our React App

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+  s3: {
+    REGION: 'us-east-1',
+    BUCKET: 'jll-serverless-tutorial'
+  },
+  apiGateway: {
+    REGION: 'us-east-1',
+    URL: 'https://m41qwmxl54.execute-api.us-east-1.amazonaws.com/prod'
+  },
+  cognito: {
+    REGION: 'us-east-1',
+    USER_POOL_ID: 'us-east-1_NmAgrCP0z',
+    APP_CLIENT_ID: '4tb9u6jmv350srqoihm4c7jkiu',
+    IDENTITY_POOL_ID: 'us-east-1:1b949028-a7d4-48a4-b66c-9f25d1eda1d0'
+  }
+};
