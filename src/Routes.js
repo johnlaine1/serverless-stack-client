6f16a3aa13bc349b87ec9f262bec9a26b33daa78
John Laine <john.laine5896@gmail.com>
2020-03-03 08:47:34 -0700

    Building our React App

--- src/Routes.js
+++ src/Routes.js
@@ -1,12 +1,50 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
+import AuthenticatedRoute from './components/AuthenticatedRoute';
+import UnauthenticatedRoute from './components/UnauthenticatedRoute';
 import Home from './containers/Home';
 import NotFound from './containers/NotFound';
+import Login from './containers/Login';
+import Signup from './containers/Signup';
+import AppliedRoute from './components/AppliedRoute';
+import NewNote from './containers/NewNote';
+import Notes from './containers/Notes';
+import Settings from './containers/Settings';
 
-export default function Routes() {
+export default function Routes({ appProps }) {
   return (
     <Switch>
-      <Route path="/" exact component={Home} />
+      <AppliedRoute path="/" exact component={Home} appProps={appProps} />
+      <AuthenticatedRoute
+        path="/settings"
+        exact
+        component={Settings}
+        appProps={appProps}
+      />
+      <UnauthenticatedRoute
+        path="/login"
+        exact
+        component={Login}
+        appProps={appProps}
+      />
+      <AuthenticatedRoute
+        path="/notes/new"
+        exact
+        component={NewNote}
+        appProps={appProps}
+      />
+      <AuthenticatedRoute
+        path="/notes/:id"
+        exact
+        component={Notes}
+        appProps={appProps}
+      />
+      <UnauthenticatedRoute
+        path="/signup"
+        exact
+        component={Signup}
+        appProps={appProps}
+      />
       <Route component={NotFound} />
     </Switch>
   );
--- src/config.js
+++ src/config.js
@@ -1,4 +1,6 @@
 export default {
+  MAX_ATTACHMENT_SIZE: 5000000,
+  STRIPE_KEY: 'pk_test_OXYYXX7t6gotXXBLKng46mLY',
   s3: {
     REGION: 'us-east-1',
     BUCKET: 'jll-serverless-tutorial'
